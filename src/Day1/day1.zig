5248f0cf5b793fa8391e3d9a3e6ab277d4feca79
SkewlOfRoot <giereck@hotmail.com>
2025-01-14 22:15:51 +0100

    Day 1 part 1

--- src/Day1/day1.zig
+++ src/Day1/day1.zig
@@ -4,10 +4,59 @@ const utils = @import("../utils.zig");
 pub fn run() !void {
     std.debug.print("Running day 1\n", .{});
 
-    const lines = try utils.readLinesStreamFromFile("src/Day1/data_test");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const lines = try utils.readLinesStreamFromFile("src/Day1/data");
     defer lines.deinit();
+
+    var listA = std.ArrayList(i32).init(allocator);
+    defer listA.deinit();
+
+    var listB = std.ArrayList(i32).init(allocator);
+    defer listB.deinit();
 
     for (lines.items) |line| {
         std.debug.print("{s}\n", .{line});
+
+        // Get first number
+        const firstNumber = try getFirstNumberFromPos(line, 0);
+        try listA.append(firstNumber);
+
+        // Get second number
+        var buff: [10]u8 = undefined;
+        const numberStr = try std.fmt.bufPrint(&buff, "{}", .{firstNumber});
+        const secondNumber = try getFirstNumberFromPos(line, numberStr.len + 3);
+        try listB.append(secondNumber);
     }
+
+    // Sort lists acending
+    try sortList(listA);
+    try sortList(listB);
+
+    // Find number distances and calculate the sum
+    var sum: u32 = 0;
+    for (0..listA.items.len) |i| {
+        const distance: u32 = @abs(listA.items[i] - listB.items[i]);
+        sum += distance;
+    }
+
+    std.debug.print("Sum: {d}\n", .{sum});
+}
+
+fn getFirstNumberFromPos(line: []const u8, pos: usize) !i32 {
+    var index: usize = pos;
+    while (index < line.len) : (index += 1) {
+        if (std.ascii.isDigit(line[index])) {
+            continue;
+        }
+        break;
+    }
+
+    const numbers = line[pos..index];
+    return try std.fmt.parseInt(i32, numbers, 10);
+}
+
+fn sortList(list: std.ArrayList(i32)) !void {
+    std.mem.sort(i32, list.items, {}, std.sort.asc(i32));
 }
